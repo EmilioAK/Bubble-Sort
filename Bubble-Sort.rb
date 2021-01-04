593f840e28837da0208109023f28bab11b7fc8cd
EmilioAK <57467368+EmilioAK@users.noreply.github.com>
2021-01-04 05:31:35 +0100

    Better version

--- Bubble-Sort.rb
+++ Bubble-Sort.rb
@@ -1,29 +1,20 @@
-def swap_values_in_list (value1, value2, target_list, index)
-    # Complicated workaround needed because Ruby sucks
-    value1, value2 = value2, value1
-    target_list[index] = value1
-    target_list[index+1] = value2
-end
-
 def sort_one_number (list)
-    working_list = list
-    was_run = false
-    
+    working_list = list.clone
+
     list.each_index do |index|
         number = working_list[index]
         next_number = working_list[index + 1]
 
         if next_number && number > next_number
-            swap_values_in_list(number, next_number, working_list, index)   
-            was_run = true
+            working_list[index], working_list[index + 1] = working_list[index + 1], working_list[index] # Can't use the variables here for some reason
         end
     end
-
-    return working_list, was_run
+    
+    return working_list, working_list == list
 end
 
 def bubble_sort (list)
     list, was_sorted = sort_one_number(list)
 
-    was_sorted ? bubble_sort(list) : (return list)
+    was_sorted ? (return list) : bubble_sort(list)
 end
